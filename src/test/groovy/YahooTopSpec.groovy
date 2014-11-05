60951ac0ddfb714f7f287deed0d75c1c25d7c071
keisatou <dummy>
2014-11-05 22:22:28 +0900

    add YahooUtil class

--- src/test/groovy/YahooTopSpec.groovy
+++ src/test/groovy/YahooTopSpec.groovy
@@ -2,6 +2,8 @@ import spock.lang.*
 import geb.spock.GebReportingSpec
 
 class YahooCommonSpec extends GebReportingSpec {
+    @Shared def hoge
+
     def "can get to the top page of Yahoo! Japan"() {
         when:
         to YahooTopPage
@@ -9,5 +11,23 @@ class YahooCommonSpec extends GebReportingSpec {
         then:
         at YahooTopPage
     }
+
+    def "hoge"() {
+        when:
+        hoge = YahooUtil.getHoge()
+
+        then:
+        hoge == 'hoge'
+    }
+
+    def "piyo"() {
+        expect:
+        hoge == 'hoge'
+    }
 }
 
+class YahooUtil {
+    public static String getHoge() {
+        return 'hoge'
+    }
+}
